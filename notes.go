4b37e280dde7365f8ae973165653951cefcd2e67
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-15 21:15:39 +0200

    setup user input

--- notes.go
+++ notes.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Go Notes")
+	fmt.Println("Go Notes")
+	title, err := getUserInput("note title: ")
+
+	if(err != nil){
+		fmt.Println(err)
+		return
+	}
+
+	content, err := getUserInput("note content: ")
+
+	if(err != nil){
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(title)
+	fmt.Println(content)
+}
+
+func getUserInput(prompt string) (string, error) {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scan(&value)
+
+	if(value == "") {
+		return "", errors.New("invalid input")
+	}
+	return value, nil
 }
