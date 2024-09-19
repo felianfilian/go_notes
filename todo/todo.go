09e2453478154e74926313173280b47ed9a8831c
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-19 03:41:02 +0200

    interface bugfixes

--- todo/todo.go
+++ todo/todo.go
@@ -3,26 +3,21 @@ package todo
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"os"
-	"strings"
-	"time"
 )
 
 type Todo struct {
-	Title     string `json:"note_title"`
 	Text   string `json:"note_content"`
-	CreatedAt time.Time `json:"note_created"`
+
 }
 
 func (todo Todo) Display() {
-	println("\n--The Note--")
-	fmt.Printf("\nNote Title: %v\nNote Content: %v\n\n", todo.Title, todo.Text)
+	println(todo.Text)
+
 }
 
 func (todo Todo) Save() error {
-	fileName := strings.ReplaceAll(todo.Title, " ", "_")
-	fileName = strings.ToLower(fileName) + ".json"
+	fileName := "todo.json"
 
 	json, err := json.Marshal(todo)
 
@@ -33,14 +28,12 @@ func (todo Todo) Save() error {
 	return os.WriteFile(fileName, json, 0644)
 }
 
-func NewNote(title, content  string) (Todo, error) {
-	if(title == "" || content == "") {
+func NewNote(content string) (Todo, error) {
+	if(content == "") {
 		return Todo{}, errors.New("invalid input")
 	}
 
 	return Todo {
-		Title: title,
 		Text: content,
-		CreatedAt: time.Now(),
 	}, nil
 }
